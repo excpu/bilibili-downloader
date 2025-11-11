fb92b6b398ae2ac6b61485c3fa2802c903a31029
Ran <kay@5share.site>
2025-11-11 00:25:46 -0500

    支持更多视频种类

--- web/index.js
+++ web/index.js
@@ -85,7 +85,11 @@ function getVideoStreams(bvid, cid, title) {
             80: "1080P 高清",
             112: "1080P+ 高码率",
             116: "1080P60 高帧率",
-            120: "超清 4K"
+            120: "超清 4K",
+            125: "HDR 真彩色",
+            126: "杜比视界",
+            127: "8K 超高清",
+            129: "HDR Vivid",
         };
         const audioIndex = {
             30216: "64K",
